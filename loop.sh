ebb2f0a112467aa717a5697375a83b5f550e979d
Qiaolun Zhang <qiaolunzhang@gmail.com>
2024-04-24 08:35:02 +0200

    add new test scripts

---
+++ loop.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+# https://www.hpc.dtu.dk/?page_id=2021
+# For more information on batch jobs, see the page on LSF jobs: https://www.hpc.dtu.dk/?page_id=1416.
+
+# Parallel Run
+# MATLAB can also run in parallel, both in a shared memory and in a distributed memory environment. If you want to run MATLAB on a single node, like on your personal computer, you have to follow the instructions for the shared memory script. This limits the number of workers that you can define in your MATLAB session to the number of cores available on a single node. Currently in the general HPC cluster we have 8 core and 20 core nodes.
+
+# Shared Memory Script
+# Assuming that your script is called MATLAB_MAINFILE .m , your script could look like this:
+
+# embedded options to bsub - start with #BSUB
+# -- our name ---
+#BSUB -J ECOC22_Op
+# -- choose queue --s
+#BSUB -q b # in espo server
+# -- specify that we need 2GB of memory per core/slot – (total requested memory is multiplied by number of requested cores)
+#BSUB -R "rusage[mem=0.5GB]"
+# -- Notify me by email when execution begins --
+#           BSUB -B
+# -- Notify me by email when execution ends   --
+#           BSUB -N
+# -- email address --
+# please uncomment the following line and put in your e-mail address,
+# if you want to receive e-mail notifications on a non-default address
+# BSUB -u qiaolun.zhang@nokia.com
+#export LSB_JOB_REPORT_MAIL=N #prevent job emails
+# -- Output File --
+#BSUB -o Output_%J.txt
+# -- Error File --
+#BSUB -e Error_%J.txt
+# -- estimated wall clock time (execution time): hh:mm --
+#       #       BSUB -W 04:00
+# -- Number of cores requested – make sure to request the minimum required to avoid blocking cores that aren’t needed (larger number of reserved cores => longer queue waiting time for all users and a down-prioritization of your next jobs according to fair runtime resources policy). FYI, if you are not using parallel toolbox (e.g., parfor) in matlab only one core is needed. If parfor as example is used in matlab, set the number of cores below as the number of works in parallel preferences settings in matlab.
+#BSUB -n 4
+# -- Specify the distribution of the cores: on a single node --
+#BSUB -R "span[hosts=1]"
+# -- end of LSF options --
+
+# -- commands you want to execute --
+#
+# If you want a specific matlab module remember to load it
+# Example: module load matlab/R2021a
+#matlab -nodisplay -batch MATLAB_MAINFILE -logfile txtLogFileName
+
+# The option #BSUB -n 4 specifies that you reserve 4 cores, and the line #BSUB -R "span[hosts=1]" specifies that these cores need to be on the same node. This means that you are reserving 4 cores for your MATLAB job, and you should not use more than 4 workers!
+
+# NOTE:
+#         If you use more workers inside matlab than cores that you have reserved, MATLAB will only run slowly! So use at most as many workers than the cores you asked for.
+#         You can get the number of cores reserved from inside MATLAB, and assign them to a variable:
+#         nw=str2num(getenv('LSB_DJOB_NUMPROC'));
+
+
+for i in 1 2 3 4 5 6 7 8
+do
+#  python3 runHeuristicIns.py $i &
+  python3 test_long_network_capacity_grooming_loop.py $i &
+  wait
+#  cust_func $i & # Put a function in the background
+done
+
+## Put all cust_func in the background and bash
+## would wait until those are completed
+## before displaying all done message
+wait
+echo "All done for all cases of opaque"
---
+++ test_long_network_capacity_grooming_loop.py
@@ -0,0 +1,277 @@
+import copy
+
+import networkx as nx
+# import winsound
+import sys
+
+import tools.Dadicated_Protection as Dadicated_Protection
+import tools.Shared_Protection as Shared_Protection
+from tools.Working import build_auxiliary_graph, serve_request
+from tools.uilts import gen_request
+from tools.Network import Continental_network
+
+
+def share_protection(topology, serve_table, approach, index):
+    G = topology
+    complexity = 0
+    spectrum = 0
+    no_serve = []
+    for i, request in enumerate(serve_table):
+        src = request[0]
+        dst = request[1]
+        rate = request[2]
+        auxiliary_graph = Shared_Protection.build_auxiliary_graph(src, dst, rate, topology, serve_table, request,
+                                                                  approach,
+                                                                  index)
+        if src in auxiliary_graph.nodes and dst in auxiliary_graph.nodes:
+            if nx.has_path(auxiliary_graph, src, dst):
+                path = nx.dijkstra_path(auxiliary_graph, src, dst)
+                consume_complexity, consume_spectrum = Shared_Protection.serve_request(G, request, path,
+                                                                                       auxiliary_graph,
+                                                                                       serve_table, approach)
+                complexity += consume_complexity
+                spectrum += consume_spectrum
+        else:
+            no_serve.append(request)
+
+    return G, no_serve
+
+
+def dadicate_protection(Germany, requests, serve_table, approach, index):
+    G = Germany
+    complexity = 0
+    spectrum = 0
+    no_serve = []
+    for request in requests:
+        src = request[0]
+        dst = request[1]
+        rate = request[2]
+        auxiliary_graph = Dadicated_Protection.build_auxiliary_graph(src, dst, rate, G, approach, index, serve_table,
+                                                                     request)
+        if src in auxiliary_graph.nodes and dst in auxiliary_graph.nodes:
+            if nx.has_path(auxiliary_graph, src, dst):
+                path = nx.dijkstra_path(auxiliary_graph, src, dst)
+                consume_complexity, consume_spectrum = Dadicated_Protection.serve_request(G, request, path,
+                                                                                          auxiliary_graph, serve_table,
+                                                                                          approach, index)
+                complexity += consume_complexity
+                spectrum += consume_spectrum
+        else:
+            no_serve.append(request)
+    # print(serve_table)
+
+    return G, no_serve
+
+
+def simulation(Germany, requests, serve_table, approach, index):
+    G = Germany.topology
+    complexity = 0
+    spectrum = 0
+    no_serve = []
+    for request in requests:
+        src = request[0]
+        dst = request[1]
+        rate = request[2]
+        auxiliary_graph = build_auxiliary_graph(src, dst, rate, G, approach, index)
+        if src in auxiliary_graph.nodes and dst in auxiliary_graph.nodes:
+            if nx.has_path(auxiliary_graph, src, dst):
+                path = nx.dijkstra_path(auxiliary_graph, src, dst)
+                consume_complexity, consume_spectrum = serve_request(G, request, path, auxiliary_graph, serve_table,
+                                                                     approach, index)
+                complexity += consume_complexity
+                spectrum += consume_spectrum
+        else:
+            no_serve.append(request)
+    # print(serve_table)
+
+    return G, no_serve
+
+
+import multiprocessing
+import time
+
+
+def run_tests(method, index, total_traffic_test, request_list_test, i_test, result):
+    no_serve_dadicated = []
+    no_serve_shared = []
+    no_serve_working = []
+    start_time = time.time()
+    print(
+        f"Testing {method} {index} for {i_test}th with {total_traffic_test / 1000}Tbs num_resuest is {len(request_list_test)}")
+    network = Continental_network()
+    route_table = {}
+    G, no_serve_working = simulation(network, request_list_test, route_table, method, index)
+    D = copy.deepcopy(G)
+    if G is False:
+        print(f"Failing {method} {index} for {total_traffic_test / 1000}Tbs in working path")
+        end_time = time.time()
+        execution_time = end_time - start_time
+        minutes, seconds = divmod(execution_time, 60)
+        # print(f"running time：{int(minutes)} min {seconds:.2f} s")
+    else:
+        if result['shared'][method][index] == 0:
+            G, no_serve_shared = share_protection(G, route_table, method, index)
+        if result['dadicated'][method][index] == 0:
+            D, no_serve_dadicated = dadicate_protection(D, request_list_test, route_table, method, index)
+    if G is False:
+        print(f"Failing {method} {index} for {total_traffic_test / 1000}Tbs in shared")
+        end_time = time.time()
+        execution_time = end_time - start_time
+        minutes, seconds = divmod(execution_time, 60)
+        # print(f"running time：{int(minutes)} min {seconds:.2f} s")
+    if D is False:
+        print(f"Failing {method} {index} for {total_traffic_test / 1000}Tbs in dadicated")
+        end_time = time.time()
+        execution_time = end_time - start_time
+        minutes, seconds = divmod(execution_time, 60)
+        # print(f"running time：{int(minutes)} min {seconds:.2f} s")
+    no_serve_Shared = no_serve_working + no_serve_shared
+    no_serve_Dadicated = no_serve_working + no_serve_dadicated
+    no_serve_share_set = set(no_serve_Shared)
+    no_serve_dadicate_set = set(no_serve_Dadicated)
+    no_serve_share_final = list(no_serve_share_set)
+    no_serve_dadicate_final = list(no_serve_dadicate_set)
+
+    no_serve_share_rate = 0
+    no_serve_dadicate_rate = 0
+    for request in no_serve_share_final:
+        no_serve_share_rate = no_serve_share_rate + request[2]
+    for request in no_serve_dadicate_final:
+        no_serve_dadicate_rate = no_serve_dadicate_rate + request[2]
+
+    end_time = time.time()
+    execution_time = end_time - start_time
+    minutes, seconds = divmod(execution_time, 60)
+    print(
+        f"running time：{int(minutes)} min {seconds:.2f} s {method} {index} for {i_test}th with {total_traffic_test / 1000}Tbs")
+
+    return no_serve_share_rate / total_traffic_test, no_serve_dadicate_rate / total_traffic_test
+
+
+def worker(methods, index_list, request_list, i, result, protection_policy, total_traffic, no_serve_rate_dict):
+    for method in methods:
+        for index in index_list:
+            if result['shared'][method][index] == 0 or result['dadicated'][method][index] == 0:
+                no_serve_shar_rate, no_serve_dadicate_rate = run_tests(method, index, total_traffic, request_list, i,
+                                                                       result)
+                no_serve_rate_dict['shared'][method][index].append(no_serve_shar_rate)
+                no_serve_rate_dict['dadicated'][method][index].append(no_serve_dadicate_rate)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        current_approach_index = int(sys.argv[1])
+    else:
+        current_approach_index = 1
+    result = {}
+    no_serve_dict = {}
+    protection_policy = ['shared', 'dadicated']
+    methods = ['SMT', 'MGDM', 'Full-MIMO', 'MF-MGDM']
+    index_list = ['Min_spectrum']
+    total_traffic = 10000  # 100000
+    for protection in protection_policy:
+        manager = multiprocessing.Manager()
+        result[protection] = manager.dict()
+
+        for method in methods:
+            result[protection][method] = manager.dict()  # 这里初始化每个方法的字典
+
+            for index in index_list:
+                result[protection][method][index] = 0
+    """result['SMT']['Min_complexity'] = 0
+    result['SMT']['Min_spectrum'] = 0
+    result['MGDM']['Min_complexity'] = 0
+    result['MGDM']['Min_spectrum'] = 0
+    result['Full-MIMO']['Min_complexity'] = 0
+    result['Full-MIMO']['Min_spectrum'] = 0
+    result['MF-MGDM']['Min_complexity'] = 0
+    result['MF-MGDM']['Min_spectrum'] = 0"""
+    result['shared']['SMT']['Min_spectrum'] = 26000
+    result['dadicated']['SMT']['Min_spectrum'] = 20000
+    result['shared']['MF-MGDM']['Min_spectrum'] = 177000  # 180000
+    result['dadicated']['MF-MGDM']['Min_spectrum'] = 111000  # 120000
+    result['shared']['MGDM']['Min_spectrum'] = 177000  # 180000
+    result['dadicated']['MGDM']['Min_spectrum'] = 111000  # 120000
+    result['shared']['Full-MIMO']['Min_spectrum'] = 211000  # 170000  # 0.96
+    result['dadicated']['Full-MIMO']['Min_spectrum'] = 161000  # 220000
+    if current_approach_index == 1:
+        result['shared']['SMT']['Min_spectrum'] = 0
+    elif current_approach_index == 2:
+        result['dadicated']['SMT']['Min_spectrum'] = 0
+    elif current_approach_index == 3:
+        result['shared']['MF-MGDM']['Min_spectrum'] = 0
+    elif current_approach_index == 4:
+        result['dadicated']['MF-MGDM']['Min_spectrum'] = 0
+    elif current_approach_index == 5:
+        result['shared']['MGDM']['Min_spectrum'] = 0
+    elif current_approach_index == 6:
+        result['dadicated']['MGDM']['Min_spectrum'] = 0
+    elif current_approach_index == 7:
+        result['shared']['Full-MIMO']['Min_spectrum'] = 0
+    elif current_approach_index == 8:
+        result['dadicated']['Full-MIMO']['Min_spectrum'] = 0
+
+
+    while True:
+        processes = []
+        for protection in protection_policy:
+            manager = multiprocessing.Manager()
+            no_serve_dict[protection] = manager.dict()
+
+            for method in methods:
+                no_serve_dict[protection][method] = manager.dict()  # 初始化每个方法的字典
+
+                for index in index_list:
+                    no_serve_dict[protection][method][index] = manager.list()
+
+        for i in range(10):
+            request_list = gen_request(total_traffic)
+            process = multiprocessing.Process(target=worker,
+                                              args=(methods, index_list, request_list, i, result, protection_policy,
+                                                    total_traffic, no_serve_dict))
+            processes.append(process)
+            process.start()
+
+        for process in processes:
+            process.join()
+
+        for protection in protection_policy:
+            for method in methods:
+                for index in index_list:
+                    print(
+                        f'***{protection}-{method}-{index}-{result[protection][method][index]}-{total_traffic / 1000}Tbs***')
+
+        for protection in protection_policy:
+            for method in methods:
+                for index in index_list:
+                    if result[protection][method][index] == 0:
+                        if 1 - sum(no_serve_dict[protection][method][index]) / len(
+                                no_serve_dict[protection][method][index]) <= 0.99:
+                            print(1 - sum(no_serve_dict[protection][method][index]) / len(
+                                no_serve_dict[protection][method][index]))
+                            result[protection][method][index] = total_traffic
+        if result['shared']['MGDM']['Min_spectrum'] == 0 or result['dadicated']['MGDM']['Min_spectrum'] ==0:
+            total_traffic += 10000
+        else:
+            total_traffic += 1000  # 确保每次循环都更新
+        # todo: 为了测试，先注释掉
+        # total_traffic += 1000  # 确保每次循环都更新
+
+        # 检查所有进程是否已完成，如果是，则退出循环
+        flag = True
+        for protection in protection_policy:
+            for method in methods:
+                for index in index_list:
+                    if result[protection][method][index] == 0:
+                        flag = False
+        if flag:
+            break
+
+    # winsound.Beep(2000, 5000)
+
+    with open(f'result\\capacity\\long_capacity_spectrum.txt', 'a+') as outfile:
+        outfile.write("\n*******************************************\n")
+        for protection in protection_policy:
+            for method in methods:
+                for index in index_list:
+                    outfile.write(f'Capacity: {protection}-{method}-{index}-{result[protection][method][index]}\n')
